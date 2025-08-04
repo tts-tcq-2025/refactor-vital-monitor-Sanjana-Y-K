c2a5d925f06494b70d74bd4d2e92f6963bf20a33
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-04 11:38:59 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,37 +1,43 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+bool isTemperatureCritical(float temperature) {
+  return temperature > 102 || temperature < 95;
+}
+
+bool isPulseRateOutOfRange(float pulseRate) {
+  return pulseRate < 60 || pulseRate > 100;
+}
+
+bool isSpo2Low(float spo2) {
+  return spo2 < 90;
+}
+
+void blinkWarningMessage(const char* message) {
+  cout << message << '\n';
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+  cout << "\r  \r" << flush;  // Clear the line after blinking
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  if (isTemperatureCritical(temperature)) {
+    blinkWarningMessage("Temperature is critical!");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  }
+  if (isPulseRateOutOfRange(pulseRate)) {
+    blinkWarningMessage("Pulse Rate is out of range!");
     return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  }
+  if (isSpo2Low(spo2)) {
+    blinkWarningMessage("Oxygen Saturation out of range!");
     return 0;
   }
   return 1;
