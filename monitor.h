e566c0506b3c1fa354453a5375e01a90c156110c
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-21 15:48:55 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,9 +1,20 @@
-#pragma once
+#ifndef MONITOR_H_
+#define MONITOR_H_
+
+#include <string>
 
 bool isTemperatureCritical(float temperature);
 bool isPulseRateOutOfRange(float pulseRate);
 bool isSpo2Low(float spo2);
 
+std::string temperatureWarning(float temperature);
+std::string pulseWarning(float pulseRate);
+std::string spo2Warning(float spo2);
+
+// Alarm blinking for critical warnings
+void blinkWarningMessage(const char* message);
+
+// Returns 1 if all vitals OK, 0 if critical alarm triggered
 int vitalsOk(float temperature, float pulseRate, float spo2);
 
-
+#endif  
