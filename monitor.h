86171b085e4742d8c4939db0f546d79b13e6f59e
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-22 14:53:25 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,31 +1,29 @@
-#ifndef MONITOR_H_
-#define MONITOR_H_
+#ifndef MONITOR_H
+#define MONITOR_H
 
 #include <string>
 
 enum class VitalStatus {
-  Normal,
-  WarningLow,
-  WarningHigh,
-  CriticalLow,
-  CriticalHigh
+    CriticalLow,
+    WarningLow,
+    Normal,
+    WarningHigh,
+    CriticalHigh
 };
 
 struct VitalCheckResult {
-  VitalStatus status;
-  std::string message;
+    VitalStatus status;
+    std::string message;
 };
 
-// Vital checks
 VitalCheckResult checkTemperature(float temperature);
+
 VitalCheckResult checkPulseRate(float pulseRate);
+
 VitalCheckResult checkSpo2(float spo2);
 
-// Shows warning or critical messages (non-blocking, no blinking)
 void printMessage(const std::string& message);
 
-// Main vitals check function
-// Returns true if all vitals normal or warning, false if any is critical
-bool vitalsOk(float temperature, float pulseRate, float spo2);
+bool vitalsOk(float temp, float pulse, float spo2);
 
-#endif  // MONITOR_H_
+#endif // MONITOR_H
