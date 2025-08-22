915b73a6b8e740bfca4ef2dc1e572523ba33d0e6
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-22 15:02:29 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,16 @@
 #pragma once
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+#include <string>
+
+enum class VitalStatus { Normal, Warning, Critical };
+
+struct VitalCheckResult {
+    VitalStatus status;
+    std::string message;
+};
+
+VitalCheckResult checkTemperature(float temperature);
+VitalCheckResult checkPulseRate(float pulseRate);
+VitalCheckResult checkSpo2(float spo2);
+
+bool vitalsOk(float temperature, float pulseRate, float spo2);
