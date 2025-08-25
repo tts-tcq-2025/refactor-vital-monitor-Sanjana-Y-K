758b03bc6b95a7007884629944b0820af7dc78fe
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-25 10:11:40 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,9 +1,9 @@
-#ifndef MONITOR_H
-#define MONITOR_H
+#pragma once
 
 int isTemperatureOk(float temperature);
 int isPulseRateOk(float pulseRate);
 int isSpO2Ok(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
 
-#endif // MONITOR_H
+
+
